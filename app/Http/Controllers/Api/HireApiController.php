276c810ac51254d8e1aa29116991b22380fac0e8
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-10-18 00:41:07 +0300

    Empty methods cleared.

--- app/Http/Controllers/Api/HireApiController.php
+++ app/Http/Controllers/Api/HireApiController.php
@@ -48,51 +48,6 @@ class HireApiController extends Controller
         ]);
     }
 
-//    /**
-//     * Store a newly created resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return \Illuminate\Http\JsonResponse
-//     */
-//    public function store()
-//    {
-//
-//    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Hire  $hire
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Hire $hire)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Hire  $hire
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Hire $hire)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Hire  $hire
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Hire $hire)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      *
